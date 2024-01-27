3ce2ef5d69b40c2e13ee2c7d6984c35a5908da2b
Maarten Menheere <m.menheere@m2tec.nl>
2024-01-27 13:45:12 +0100

    Double escape environment variables with 105366

--- scripts/up.sh
+++ scripts/up.sh
@@ -22,11 +22,11 @@ export OGMIOS_VERSION=latest
 export CARDANO_NODE_VERSION=8.1.2 # https://github.com/IntersectMBO/cardano-node/releases/tag/8.1.2
 export CARDANO_DB_SYNC_VERSION=13.1.1.3
 export SWAGGER_API_URL=http://127.0.0.1:3000/  # public / internal postgrest exposed host
-export TOKEN_REGISTRY_GIT_REPO=https://github.com/cardano-foundation/cardano-token-registry
-export TOKEN_REGISTRY_GIT_BRANCH=master
-export TOKEN_REGISTRY_DEST=/app/cardano-token-registry
-export TOKEN_REGISTRY_SYNC_INTERVAL=60 #3600s=1h
-export TOKEN_REGISTRY_MAPPINGS_DIR=$TOKEN_REGISTRY_DEST/mappings
+# export TOKEN_REGISTRY_GIT_REPO=https://github.com/cardano-foundation/cardano-token-registry
+# export TOKEN_REGISTRY_GIT_BRANCH=master
+# export TOKEN_REGISTRY_DEST=/app/cardano-token-registry
+# export TOKEN_REGISTRY_SYNC_INTERVAL=60 #3600s=1h
+# export TOKEN_REGISTRY_MAPPINGS_DIR=$TOKEN_REGISTRY_DEST/mappings
 
 echo "Initializing $PROJECT_NAME with this params:" &&
 echo "NETWORK	= $NETWORK" &&
@@ -60,7 +60,7 @@ echo "TOKEN_REGISTRY_MAPPINGS_DIR	= $TOKEN_REGISTRY_MAPPINGS_DIR" &&
 # EMAIL=$EMAIL \
 # NETWORK=$NETWORK \
 # export EMAIL_SANITIZED=$EMAIL_SANITIZED \
-TOKEN_REGISTRY_GIT_REPO=$TOKEN_REGISTRY_GIT_REPO \
-TOKEN_REGISTRY_GIT_BRANCH=$TOKEN_REGISTRY_GIT_BRANCH \
+# TOKEN_REGISTRY_GIT_REPO=$TOKEN_REGISTRY_GIT_REPO \
+# TOKEN_REGISTRY_GIT_BRANCH=$TOKEN_REGISTRY_GIT_BRANCH \
 docker compose -p $PROJECT_NAME --env-file docker-${NETWORK}.env up -d &&
 docker compose -p $PROJECT_NAME logs -f
