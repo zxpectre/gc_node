7c640338e19ba41e5092fad70be9e0a9d46580c0
Maarten Menheere <m.menheere@m2tec.nl>
2024-01-30 09:44:01 +0100

    Add postgrest-gc service

--- scripts/up.sh
+++ scripts/up.sh
@@ -10,7 +10,6 @@ PROJECT_NAME=gc-node-${NETWORK}
 echo "Initializing $PROJECT_NAME with this params:" &&
 echo "NETWORK	= $NETWORK" &&
 cat docker-${NETWORK}.env
-cat token-reg.env
 
 docker compose -p $PROJECT_NAME --env-file docker-${NETWORK}.env up -d &&
 docker compose -p $PROJECT_NAME logs -f
